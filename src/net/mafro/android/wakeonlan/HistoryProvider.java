fdfddab4c4bd512d44eae9f9717a558e130d64a3
mafro <dev@mafro.net>
2009-06-21 13:31:08 +0100

    Log call removed from onUpgrade

--- src/net/mafro/android/wakeonlan/HistoryProvider.java
+++ src/net/mafro/android/wakeonlan/HistoryProvider.java
@@ -62,8 +62,6 @@ public class HistoryProvider extends ContentProvider {
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
-                    + newVersion + ", which will destroy all old data");
             db.execSQL("DROP TABLE IF EXISTS notes");
             onCreate(db);
         }
