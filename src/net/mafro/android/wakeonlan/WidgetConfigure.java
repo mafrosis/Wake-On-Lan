cd3b1f5ed67950ede5c521d1e6929fdd444c3704
Yohan Pereira <yohan.pereira@gmail.com>
2013-07-15 15:57:04 +0530

    added homescreen widget

---
+++ src/net/mafro/android/wakeonlan/WidgetConfigure.java
@@ -0,0 +1,105 @@
+/*
+Copyright (C) 2013 Yohan Pereira.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors may be used
+  to endorse or promote products derived from this software without specific
+  prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+package net.mafro.android.wakeonlan;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+
+import android.appwidget.AppWidgetManager;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.Context;
+
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RemoteViews;
+
+/**
+ * @desc This class is used to configure the home screen widget.
+ */
+public class WidgetConfigure extends Activity
+{
+	private HistoryListHandler historyListHandler;
+	private int widget_id;
+	private SharedPreferences settings;
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.widget_configure);
+		ListView lv = (ListView)findViewById(R.id.history);
+		historyListHandler = new HistoryListHandler(this, lv);
+
+		settings = getSharedPreferences(WakeOnLanActivity.TAG, 0);
+		int sort_mode = settings.getInt("sort_mode", WakeOnLanActivity.CREATED);
+		historyListHandler.bind(sort_mode);
+		//Add on click listener
+		historyListHandler.addHistoryListClickListener(new HistoryListClickListener () {
+
+			public void onClick(HistoryItem item) {
+				selected(item);
+			}
+		});
+
+		//get the widget id
+		Intent intent = getIntent();
+		widget_id = WidgetProvider.getWidgetId(intent);
+
+		if(widget_id == AppWidgetManager.INVALID_APPWIDGET_ID) {
+			//No valid widget id .. bailling.
+			finish();
+		}
+
+	}
+
+	private void selected(HistoryItem item) {
+		// save selected item id to the settings.
+		WidgetProvider.saveItemPref(settings, item, widget_id);	
+
+		// configure the widget
+		WidgetProvider.configureWidget(widget_id, item, this);
+
+		//return result
+		Intent resultValue = new Intent();
+		resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widget_id);
+            	setResult(RESULT_OK, resultValue);
+            	finish();
+
+	}
+
+	
+	
+}
---
+++ src/net/mafro/android/wakeonlan/WidgetProvider.java
@@ -0,0 +1,202 @@
+/*
+Copyright (C) 2013 Yohan Pereira.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors may be used
+  to endorse or promote products derived from this software without specific
+  prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+package net.mafro.android.wakeonlan;
+
+
+import android.app.PendingIntent;
+
+import android.database.Cursor;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import android.os.Bundle;
+
+import android.widget.RemoteViews;
+import android.util.Log;
+
+/**
+ * @desc This class is used to setup the home screen widget, as well as handle click events
+ * @rename WidgetProvider
+ */
+
+//TODO handle deletion of widgets.
+
+public class WidgetProvider extends AppWidgetProvider {
+
+	public static final String SETTINGS_PREFIX="widget_";
+	public static final String WIDGET_ONCLICK="net.mafro.android.wakeonlan.WidgetOnClick";
+
+	private SharedPreferences settings;
+
+	public WidgetProvider() {
+		Cursor cursor;
+		//Need a context to initlise this.
+		settings = null;
+	}
+
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		if(settings == null) {
+			settings = context.getSharedPreferences(WakeOnLanActivity.TAG, 0);
+		}
+
+		// Perform this loop procedure for each widget.
+		final int N = appWidgetIds.length;
+		for (int i=0; i<N; i++) {
+			//TODO initlise the widget here.
+			int widget_id = appWidgetIds[i];
+			
+			
+			HistoryItem item = loadItemPref(context, settings, widget_id);
+			if(item == null) { // item or prefrences missing.
+				//TODO delete the widget probably.
+				continue;
+			}
+			configureWidget(widget_id, item, context);
+		}
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		//
+		super.onReceive(context, intent);
+		Log.v(WakeOnLanActivity.TAG, "in onReceive");
+
+		if(settings == null) {
+			settings = context.getSharedPreferences(WakeOnLanActivity.TAG, 0);
+		}
+
+		if (intent.getAction().startsWith(WIDGET_ONCLICK)) {
+
+			//get the widget id
+			int widget_id = getWidgetId(intent);
+			if(widget_id == AppWidgetManager.INVALID_APPWIDGET_ID) {
+				return;
+			}
+			
+			//get the HistoryItem associated with the widget_id.
+			HistoryItem item = loadItemPref(context, settings, widget_id);
+
+			WakeOnLanActivity.sendPacket(context, 
+						     item.title, 
+						     item.mac, 
+						     item.ip, 
+						     item.port);
+		}
+	}
+
+
+	/**
+	 * @desc gets the widget id from an intent.
+	 */
+	public static int getWidgetId(Intent intent) {
+		Bundle extras = intent.getExtras();
+		int _widget_id = AppWidgetManager.INVALID_APPWIDGET_ID;
+		if (extras != null) {
+			_widget_id = extras.getInt(
+					AppWidgetManager.EXTRA_APPWIDGET_ID, 
+					AppWidgetManager.INVALID_APPWIDGET_ID);
+
+		}
+		return _widget_id;	
+	}
+
+	/**
+	 * @desc configures a widget for the first time. Usually called when creating a widget
+	 *	 for the first time or initlising existing widgets when the AppWidgetManager 
+	 *	restarts (usually when the phone reboots).
+	 */
+	public static void configureWidget(int widget_id, HistoryItem item, Context context) {
+
+		RemoteViews views = new RemoteViews(context.getPackageName(),
+		R.layout.widget);
+		views.setTextViewText(R.id.appwidget_text, item.title);
+		//FIXME hack : append id to action  to prevent clearing the extras bundle. 
+		views.setOnClickPendingIntent(R.id.appwidget_button, getPendingSelfIntent(context, widget_id, WIDGET_ONCLICK + widget_id));
+
+		//tell the widget manager
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+		appWidgetManager.updateAppWidget(widget_id, views);
+	}
+	
+	
+
+	private static  PendingIntent getPendingSelfIntent(Context context, int widget_id, String action) {
+		//TODO try and set the widget id here. (not sure what this means anymore).
+		Intent intent = new Intent(context, WidgetProvider.class);
+		intent.setAction(action);
+		Bundle bundle = new Bundle();
+		bundle.putInt(AppWidgetManager.EXTRA_APPWIDGET_ID, widget_id);
+		intent.putExtras(bundle);
+		return PendingIntent.getBroadcast(context, 0, intent, 0);
+	}
+	
+	/**
+	 * @desc saves the given history item/widget_id combination.
+	 */
+	public static void saveItemPref(SharedPreferences settings, HistoryItem item, int widget_id) {
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putInt(SETTINGS_PREFIX + widget_id, item.id);
+		editor.commit();
+	}
+
+	/**
+	 * @desc load the HistoryItem associated with a widget_id.
+	 */
+	public static HistoryItem loadItemPref(Context context, SharedPreferences settings, int widget_id) {
+		//get item_id
+		int item_id = settings.getInt(SETTINGS_PREFIX + widget_id, -1);
+		if(item_id == -1) {
+			//No item_id found for given widget return null.
+			return null;
+		}
+
+		//Create HistoryItem
+		Cursor cursor = context.getContentResolver()
+				.query(History.Items.CONTENT_URI, 
+					HistoryListHandler.PROJECTION, 
+					History.Items._ID +" = "+item_id,
+					null, null);
+		if(cursor.getCount() == 0) {
+			//item_id does not exists anymore
+			//TODO delete the prefrence.
+			return null;
+		}
+		cursor.moveToFirst();
+		HistoryItem item = HistoryListHandler.getItem(cursor);
+		return item;
+
+	}
+	
+}
